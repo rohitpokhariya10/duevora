d96a359ce246c98f069b65c866291a73d05860be
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-16 03:53:46 +0530

    added the errors and responses classe

---
+++ server/src/shared/errors/TooManyRequests.error.js
@@ -0,0 +1,21 @@
+// Importing modules
+import ApiError from "../utils/ApiError.util.js";
+import HTTP_STATUS from "../constants/StatusCodes.constants.js";
+
+// class for NotFound error
+class Forbidden extends ApiError {
+
+    // constructor to initialize the error class
+    constructor(message) {
+
+        // calling the parent class constructor
+        super(HTTP_STATUS.TOO_MANY_REQUESTS, message);
+
+        // setting the status code and message for the error
+        this.message = message;
+
+    }
+
+}
+
+export default Forbidden;
---
+++ server/src/shared/errors/UnProcessableEntity.error.js
@@ -0,0 +1,21 @@
+// Importing modules 
+import ApiError from "../utils/ApiError.util.js";
+import HTTP_STATUS from "../constants/StatusCodes.constants.js";
+
+// class for UnProcessableEntity error
+class UnProcessableEntity extends ApiError {
+
+    // constructor to initialize the error class
+    constructor(message) {
+
+        // calling the parent class constructor
+        super(HTTP_STATUS.UNPROCESSABLE_ENTITY, message);
+
+        // setting the status code and message for the error
+        this.message = message;
+
+    }
+
+}
+
+export default UnProcessableEntity;
