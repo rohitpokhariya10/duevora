0ccd6c93760c77eca01dfd2597795a9f9cce9c95
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-19 22:11:48 +0530

    fix: skip pino-pretty transport in production (devDependency only)

--- server/src/shared/config/logger.config.js
+++ server/src/shared/config/logger.config.js
@@ -5,14 +5,16 @@ import env from "./env.config.js";
 // creating a logger instance
 const logger = pino({
     level: env.NODE_ENV === "production" ? "info" : "debug",
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "SYS:standard",
-            ignore: "pid,hostname",
+    ...(env.NODE_ENV !== "production" && {
+        transport: {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+            },
         },
-    },
+    }),
 });
 
 export default logger;
